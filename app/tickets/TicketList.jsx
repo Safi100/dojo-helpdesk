71448f66cd0ec81dc4360c4c1b13e7edacab066d
Safi100 <safinafi12@gmail.com>
2025-05-28 22:34:23 +0300

    Update ticket list component

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -3,14 +3,16 @@ import Link from "next/link";
 async function getTickets() {
   const res = await fetch("http://localhost:4000/tickets", {
     next: {
-      revalidate: 0,
+      revalidate: 0, // use 0 to opt out of using cache
     },
   });
+
   return res.json();
 }
 
 export default async function TicketList() {
   const tickets = await getTickets();
+
   return (
     <>
       {tickets.map((ticket) => (
@@ -19,13 +21,13 @@ export default async function TicketList() {
             <h3>{ticket.title}</h3>
             <p>{ticket.body.slice(0, 200)}...</p>
             <div className={`pill ${ticket.priority}`}>
-              {ticket.priority} Priority
+              {ticket.priority} priority
             </div>
           </Link>
         </div>
       ))}
       {tickets.length === 0 && (
-        <p className="text-center">No open tickets found.</p>
+        <p className="text-center">There are no open tickets.</p>
       )}
     </>
   );

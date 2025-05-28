97d2b5f534ce261ad65fe0537dd110580cb45121
Safi100 <safinafi12@gmail.com>
2025-05-28 22:34:06 +0300

    Update

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,17 +1,25 @@
+import { Suspense } from "react";
 import TicketList from "./TicketList";
+import Loading from "../loading";
+import Link from "next/link";
 
 export default function Tickets() {
   return (
     <main>
       <nav>
         <div>
-          <h2>Ticket</h2>
+          <h2>Tickets</h2>
           <p>
             <small>Currently open tickets.</small>
           </p>
         </div>
+        <Link href="/tickets/create" className="ml-auto">
+          <button className="btn-primary">New Ticket</button>
+        </Link>
       </nav>
-      <TicketList />
+      <Suspense fallback={<Loading />}>
+        <TicketList />
+      </Suspense>
     </main>
   );
 }
